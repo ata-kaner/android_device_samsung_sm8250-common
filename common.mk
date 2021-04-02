d7c6c5f3d921d3bd3388940746e5b515a934dc44
Hridya Valsaraju <hridya@google.com>
2021-04-02 11:33:45 -0700

    sm8250-common: Set PRODUCT_SET_DEBUGFS_RESTRICTIONS
    Starting with Android R launched devices, debugfs cannot be mounted in
    production builds. In order to avoid accidental debugfs dependencies
    from creeping in during development with userdebug/eng builds, the
    build flag PRODUCT_SET_DEBUGFS_RESTRICTIONS can be set by vendors to
    enforce additional debugfs restrictions for userdebug/eng builds. The
    same flag will be used to enable sepolicy neveallow statements to
    prevent new permissions added for debugfs access.
    
    Test: build, boot
    Bug: 184381659
    Change-Id: I45e6f20c886d467a215c9466f3a09965ff897d7e

--- common.mk
+++ common.mk
@@ -246,6 +246,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
 
+# Kernel
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung \

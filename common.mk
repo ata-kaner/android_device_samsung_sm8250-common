9641769bff877956f3646f0ff82031314bb28dc4
Tim Zimmermann <tim@linux4.de>
2022-04-17 16:40:02 +0200

    sm8250-common: Set SEC specific BT SCO sample rate audio param
    This lets us convert AOSP's bt_wbs to SEC's g_sco_samplerate.
    
    Change-Id: I78d5d36044421daf99b8a13aa417060a3cf959a3

--- common.mk
+++ common.mk
@@ -386,7 +386,7 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libxml2 \

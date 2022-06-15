8d9f4137b3f48d95a3d1907a2b17db1f059663fd
Rashed Abdel-Tawab <rashed@linux.com>
2022-06-15 14:45:20 +0200

    r8q: Import audio@7.0 default impl
    Change-Id: I37dac8d63dff454a396e9c14a1062db7f4b2588d

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@6.0-impl.samsung-sm8250 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \

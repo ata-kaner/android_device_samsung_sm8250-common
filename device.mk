d5a17ebef50e6d50f99635ae846f4cd0cf8ca2e7
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-04-07 14:59:36 +0200

    r8q: Import recovery ramdisk
    Change-Id: Iaeefbe107a52249934aa689e7d506ed5c9c7c827

--- device.mk
+++ device.mk
@@ -350,6 +350,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \

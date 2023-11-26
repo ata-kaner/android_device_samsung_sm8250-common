2a6e6ff4554ebc256d93e2be83abb5c2ff9b68c6
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-11-26 22:04:27 +0100

    r8q: overlay: Setting up UDFPS
    * Fixes FOD screen off.
    
    Change-Id: Ib3f57c7f2b25fcb69ad5a83597bab3a707bc49ff

--- device.mk
+++ device.mk
@@ -362,7 +362,10 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+     $(DEVICE_PATH)/overlay \
+     $(DEVICE_PATH)/overlay-lineage
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/samsung/r8q/overlay/hardware/samsung/AdvancedDisplay

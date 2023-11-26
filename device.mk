0600028360e24276c3902da254f72aaa857c828c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-26 16:08:30 +0100

    r8q: Setup `TARGET_HAS_UDFPS` flag
    Change-Id: I00ce6ea4e8efa66eeb7ea80f9c5d0179e6299671

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ $(call inherit-product, vendor/samsung/r8q/r8q-vendor.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # AdvancedDisplay (MDNIE)
 PRODUCT_PACKAGES += \
     AdvancedDisplay

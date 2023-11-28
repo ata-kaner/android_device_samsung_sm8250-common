5f9f61e4b8c90b1f152fa8398861abcdb31e2cd2
Marc Bourgoin <themard85@gmail.com>
2023-11-28 21:21:54 +0900

    Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    SEPolicy.mk placed in the root automatically redirects devices to the
    proper subfolder.
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,7 +234,7 @@ BOARD_VENDOR := samsung
 VENDOR_SECURITY_PATCH := 2023-09-01
 
 # SePolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private

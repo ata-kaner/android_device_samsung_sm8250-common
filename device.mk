81aa82dade38853422cfc82201b10f4828eaed80
ata-kaner <ata_kaner@outlook.com>
2023-11-28 04:46:57 +0300

    r8q: Remove audio hal with no commit history

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl.sm8250 \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \

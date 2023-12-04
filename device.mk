b9104d8ed4396f1db358da120ecc0b553a1ee866
ata-kaner <ata_kaner@outlook.com>
2023-12-04 23:57:21 +0300

    r8q: Build radio.config@1.2
    * RIL stack dependency

--- device.mk
+++ device.mk
@@ -470,7 +470,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     librilutils \
     librmnetctl \

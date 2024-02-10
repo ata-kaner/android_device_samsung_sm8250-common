e29002f65816c7ac0fb39c655e70e6aea9036fb3
ata-kaner <ata_kaner@outlook.com>
2024-02-10 20:22:35 +0300

    sm8250-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- common.mk
+++ common.mk
@@ -439,6 +439,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.samsung-multihal \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     sensors.samsung_sm8250 \

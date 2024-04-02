Xtended-Devices/android_device_realme_sm8250-common

sm8250-common: Import task_profiles.json
Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

common.mk
@@ -353,6 +353,9 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 

ata-kaner/android_device_samsung_sm8250-common

Change-Id: I5cdb948a474b5e65959a67d537a1244eb2408e17

common.mk
@@ -313,6 +313,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
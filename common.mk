ata-kaner/android_device_samsung_sm8250-common

commit 8269a76f23ea7722aa114243d3b45508ca844dbb

sm8250-common: switch to AIDL fingerprint HAL

common.mk
@@ -210,7 +210,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung \
+    android.hardware.biometrics.fingerprint-service.samsung \
     android.hardware.common-V1-ndk_platform.vendor \
     fingerprint.default
 

BoardConfigCommon.mk
@@ -160,10 +160,6 @@ AUDIO_FEATURE_ENABLED_SSR := false
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 BOARD_USES_ALSA_AUDIO := true
 
-# Fingerprint
-TARGET_SEC_FP_REQUEST_FORCE_CALIBRATE := true
-TARGET_SEC_FP_REQUEST_TOUCH_EVENT := true
-
 # FOD
 TARGET_SURFACEFLINGER_UDFPS_LIB := //$(COMMON_PATH):libudfps_extension.samsung-sm8250
 TARGET_USES_FOD_ZPOS := true

configs/framework_compatibility_matrix.xml
@@ -57,14 +57,6 @@
         </interface>
     </hal>
     <hal format="hidl" optional="true">
-        <name>android.hardware.biometrics.fingerprint</name>
-        <version>2.3</version>
-        <interface>
-            <name>IBiometricsFingerprint</name>
-            <instance>default</instance>
-        </interface>
-    </hal>
-    <hal format="hidl" optional="true">
         <name>android.hardware.gatekeeper</name>
         <version>1.0</version>
         <interface>
@@ -497,14 +489,6 @@
         <version>1.0-1</version>
         <interface>
             <name>IFactory</name>
-            <instance>default</instance>
-        </interface>
-    </hal>
-    <hal format="hidl" optional="true">
-        <name>vendor.qti.hardware.fingerprint</name>
-        <version>1.0</version>
-        <interface>
-            <name>IQtiExtendedFingerprint</name>
             <instance>default</instance>
         </interface>
     </hal>
@@ -987,14 +971,6 @@
         <version>2.0</version>
         <interface>
             <name>ISehBiometricsFace</name>
-            <instance>default</instance>
-        </interface>
-    </hal>
-    <hal format="hidl" optional="true">
-        <name>vendor.samsung.hardware.biometrics.fingerprint</name>
-        <version>3.0</version>
-        <interface>
-            <name>ISehBiometricsFingerprint</name>
             <instance>default</instance>
         </interface>
     </hal>

configs/manifest.xml
@@ -61,16 +61,6 @@
         <fqname>@1.0::IBiometricsFace/default</fqname>
     </hal>
     <hal format="hidl">
-        <name>android.hardware.biometrics.fingerprint</name>
-        <transport>hwbinder</transport>
-        <version>2.3</version>
-        <interface>
-            <name>IBiometricsFingerprint</name>
-            <instance>default</instance>
-        </interface>
-        <fqname>@2.3::IBiometricsFingerprint/default</fqname>
-    </hal>
-    <hal format="hidl">
         <name>android.hardware.bluetooth</name>
         <transport>hwbinder</transport>
         <version>1.0</version>
@@ -425,16 +415,6 @@
             <instance>default</instance>
         </interface>
         <fqname>@2.0::ISehBiometricsFace/default</fqname>
-    </hal>
-    <hal format="hidl">
-        <name>vendor.samsung.hardware.biometrics.fingerprint</name>
-        <transport>hwbinder</transport>
-        <version>3.0</version>
-        <interface>
-            <name>ISehBiometricsFingerprint</name>
-            <instance>default</instance>
-        </interface>
-        <fqname>@3.0::ISehBiometricsFingerprint/default</fqname>
     </hal>
     <hal format="hidl">
         <name>vendor.samsung.hardware.bluetooth</name>

overlay/frameworks/base/core/res/res/values/config.xml
@@ -1289,14 +1289,6 @@
          on the headphone/microphone jack. When false use the older uevent framework. -->
     <bool name="config_useDevInputEventForAudioJack">true</bool>
 
-    <!-- List of biometric sensors on the device, in decreasing strength. Consumed by AuthService
-         when registering authenticators with BiometricService. Format must be ID:Modality:Strength,
-         where: IDs are unique per device, Modality as defined in BiometricAuthenticator.java,
-         and Strength as defined in Authenticators.java -->
-    <string-array name="config_biometric_sensors" translatable="false" >
-        <item>0:2:15</item>
-    </string-array>
-
     <!-- Whether the always on display mode is available. This should only be enabled
          on devices where the display has been tuned to be power efficient in DOZE and/or
          DOZE_SUSPEND states. -->
@@ -1365,9 +1357,6 @@
     <!-- Whether device supports double tap to wake -->
     <bool name="config_supportDoubleTapWake">true</bool>
 
-    <!-- For performance and storage reasons, limit the number of fingerprints per user -->
-    <integer name="config_fingerprintMaxTemplatesPerUser">4</integer>
-
     <!-- Doze: should the TYPE_PICK_UP_GESTURE sensor be used as a pulse signal. -->
     <bool name="config_dozePulsePickup">true</bool>
 
@@ -1404,18 +1393,6 @@
          @see https://www.w3.org/TR/SVG/paths.html#PathData
          -->
     <string translatable="false" name="config_mainBuiltInDisplayCutout">M 0,0 M 0,12.25101276465640 a 10.60613009248640,10.60613009248640 0 1,0 0,21.21226018497290 a 10.60613009248640,10.60613009248640 0 1,0 0,-21.21226018497290 Z @dp</string>
-
-    <!-- The properties of a UDFPS sensor in pixels, in the order listed below: -->
-    <integer-array name="config_udfps_sensor_props" translatable="false" >
-    <!--
-        <item>sensorLocationX</item>
-        <item>sensorLocationY</item>
-        <item>sensorRadius</item>
-    -->
-        <item>540</item>
-        <item>2137</item>
-        <item>119</item>
-    </integer-array>
 
     <!-- The RadioAccessFamilies supported by the device.
          Empty is viewed as "all".  Only used on devices which

sepolicy/vendor/file_contexts
@@ -80,7 +80,7 @@
 /(vendor|system/vendor)/bin/hw/macloader                                                                    u:object_r:macloader_exec:s0
 
 # Hals
-/(vendor|system/vendor)/bin/hw/android.hardware.biometrics.fingerprint@2.3-service.samsung                  u:object_r:hal_fingerprint_default_exec:s0
+/(vendor|system/vendor)/bin/hw/android.hardware.biometrics.fingerprint-service.samsung                      u:object_r:hal_fingerprint_default_exec:s0
 /(vendor|system/vendor)/bin/hw/android\.hardware\.camera\.provider@2\.6-service\_64\.samsung\-sm8250        u:object_r:hal_camera_default_exec:s0
 /(vendor|system/vendor)/bin/hw/android\.hardware\.drm@[0-9]\.[0-9]-service\.widevine                        u:object_r:hal_drm_widevine_exec:s0
 /(vendor|system/vendor)/bin/hw/android\.hardware\.health@2\.1-service-samsung                               u:object_r:hal_health_default_exec:s0

vendor.prop
@@ -159,6 +159,11 @@ vendor.display.idle_time=0
 vendor.display.use_smooth_motion=0
 vendor.gralloc.disable_ubwc=0
 
+# Fingerprint
+ro.vendor.fingerprint.type=udfps_optical
+ro.vendor.fingerprint.sensor_location=540|2137|119
+ro.vendor.fingerprint.request_touch_event=true
+
 # Gatekeeper
 ro.hardware.gatekeeper=mdfpp
 

commit de3653845012a903d201e3fc1b95538563288518

sm8250-common: Stop defining board api level
When config_biometric_sensors is not defined, board API level triggers a backward compatibility in AuthService.java for R vendors. This breaks our AIDL fingerprint and crashes system server due to duplicate registration with same ID's.

https://android.googlesource.com/platform/frameworks/base/+/master/services/core/java/com/android/server/biometrics/AuthService.java#705

Stop defining board API level so our fingerprint HAL can finally register.

Fixes: 8269a76f23ea7722aa114243d3b45508ca844dbb "sm8250-common: switch to AIDL fingerprint HAL"

Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

common.mk
@@ -24,10 +24,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Board API Level
-BOARD_SHIPPING_API_LEVEL := 30
-BOARD_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
-
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
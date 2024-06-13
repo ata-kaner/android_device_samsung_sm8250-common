17b38aaad6bd3b569856568c4859c8c09faf264d
ata-kaner <ata.kaner03@gmail.com>
2024-06-13 23:53:23 +0300

    sm8350-common: Update kernel cmdline firmware path
    Looks like lahaina didn't drop /vendor/firmware_mnt
    
    Change-Id: I5e5b92bb32a01a726c9aeef0cf4a8dc1ec146ec0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,7 @@ TARGET_KERNEL_HEADER_ARCH   := arm64
 TARGET_LINUX_KERNEL_VERSION := 5.4
 
 # Kernel flags
-BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4
+BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_BOOT_HEADER_VERSION := 3
 

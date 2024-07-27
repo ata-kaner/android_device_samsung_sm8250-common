2d9853e3372a3a55c7afe81966bd335c39750b04
Andrzej Szafran <rtd1250@gmail.com>
2024-07-27 13:19:57 +0200

    sm8250-common: Include hardware/samsung FCM
    Change-Id: Icb1a1bd6de56cbf2419cb8a046fa2a7a35cfc912

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,6 +178,7 @@ TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_samsung_sm8250
 # HIDL manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml

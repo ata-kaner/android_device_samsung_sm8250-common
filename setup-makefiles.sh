8b5ff3ec4bbec69440627ca9faeb74a21168eff3
ata-kaner <ata.kaner03@gmail.com>
2024-08-18 15:48:51 +0300

    sm8250-common: Exclude unnecessary blobs from non-cellular tablet builds
    Change-Id: I8fdf213170b57a124dc865a93991043faabc2893

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -30,6 +30,13 @@ write_headers "gts7l gts7lwifi gts7xl gts7xlwifi r8q"
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
+# Exclude blobs from non-cellular tablet builds
+printf '\n%s\n' 'ifneq ($(TARGET_HAS_NO_RIL),true)' >> "$PRODUCTMK"
+
+write_makefiles "${MY_DIR}/proprietary-files-radio.txt" true
+
+printf '%s\n' 'endif' >> "$PRODUCTMK"
+
 # Finish
 write_footers
 
